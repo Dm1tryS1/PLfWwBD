27afac34c4e0ce23f922d30667733c2aadc8024c
Dmitry Malkov <dvm101@yandex.ru>
2024-02-18 18:41:48 +0300

    #Lab4 fix

--- src/main/kotlin/second_task/SecondTask.kt
+++ src/main/kotlin/second_task/SecondTask.kt
@@ -1,27 +1,33 @@
 package org.example.second_task
 
 fun main() {
-    val theater = Theater("Big Theater", false)
+    val theater = Theater("Theatre Square, 1", false, "Big Theater")
 
     theater.changeLightsState(true)
     theater.changeState(true)
 }
 
-class Theater(override val address: String, override var state: Boolean) : PublicBuilding(address, state) {
+class Theater(override val address: String, override var state: Boolean, override val name: String) :
+    PublicBuilding(address, state, name) {
     override fun changeState(newState: Boolean) {
         super.changeState(newState)
         println("New state: $state")
     }
 
-    override fun changeLightsState(state: Boolean) =
-        println("Lights ${if (state) java.lang.String("on") else java.lang.String("off")}")
-
 }
 
-abstract class PublicBuilding(override val address: String, protected open var state: Boolean) : Building {
+abstract class PublicBuilding(
+    override val address: String,
+    protected open var state: Boolean,
+    protected open val name: String
+) : Building {
     open fun changeState(newState: Boolean) {
         state = newState
     }
+
+    override fun changeLightsState(state: Boolean) =
+        println("Lights ${if (state) java.lang.String("on") else java.lang.String("off")}")
+
 }
 
 interface Building {
