6828526eb5f88d7c4b604fcc093e40fe13c2b693
Dmitry Malkov <dvm101@yandex.ru>
2024-02-27 20:43:09 +0300

    #Lab9 done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,32 @@
 package org.example
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
+    firstTask()
+    secondTask()
+    thirdTask()
+    fourthTask()
 }
+
+fun firstTask() {
+    val result = INPUT_STRING.split(" ").stream().limit(2).reduce { t, u -> "$t $u" }.get()
+    println(result)
+}
+
+fun secondTask() {
+    val result = INPUT_STRING.split(" ").stream().skip(1).limit(2).reduce { t, u -> "$t $u" }.get()
+    println(result)
+}
+
+fun thirdTask() {
+    val result = listOf("a1", "a2", "a3", "a1").stream().map { it.removeRange(0, 1) }.toList()
+    println(result)
+}
+
+fun fourthTask() {
+    val result = INPUT_STRING.split(" ").stream().map { "${it}_task14"}.toList()
+    println(result)
+}
+
+const val INPUT_STRING = "Hello! This is new task"
